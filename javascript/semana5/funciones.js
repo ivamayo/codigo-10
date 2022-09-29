e96c21fd4c2cc7569a3e08f90562abd1f1ec477d
Ivan <ivanmayo1785@gmail.com>
2022-09-29 11:35:12 -0500

    avance

---
+++ javascript/semana5/funciones.js
@@ -0,0 +1,20 @@
+// Sintaxys: La forma en la que escribimos escribimos el codigo
+
+// function sumar() {
+
+//}
+let n3 = 30;
+
+function sumar() {
+  // codigo
+  let n1 = 10;
+  let n2 = 20;
+  console.log(n1 + n2 + n3);
+}
+
+let n4 = 40;
+
+console.log(n1);
+console.log(n3);
+
+let nombre = "Pepe";
